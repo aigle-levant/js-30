b8e4294c6ee4fc125e29339eb16768a67c8c0710
aigle-levant <aiglelevant@gmail.com>
2024-10-14 12:09:06 +0530

    added day 5 notes

--- assets/day-five/source/day-five.js
+++ assets/day-five/source/day-five.js
@@ -1,1 +1,11 @@
 //AIGLE
+
+function toggleClass(event)
+{
+    let element = event.target;
+    element.classList.toggle('after-change');
+    element.classList.toggle('before-change');
+}
+let panels = document.querySelectorAll('.image');
+
+panels.forEach(image => image.addEventListener('click', toggleClass));
